65cd0c1a3f431e2f3d428685c528a8baa1a93e2f
Simona <see.moona@yahoo.com>
2020-04-30 16:05:46 +0200

    update function

--- script/index.js
+++ script/index.js
@@ -14,21 +14,32 @@ function displayDescr(event) {
   this.style.backgroundColor = "#015668";
   this.style.color = "#ffd369";
   descriptionBox.classList.add("displayDescription");
-  if (event.target == items[0]) {
-    posterImg.style.backgroundImage = "url('../img/1-Friends.jpg')";
-  } else if (event.target == items[1]) {
-    posterImg.style.backgroundImage = "url('../img/2-brother.jpg')";
-    movieDescription.textContent = "In the deep south during the 1930s, three escaped convicts search for hidden treasure while a relentless lawman pursues them."
-  } else if (event.target == items[2]) {
-    posterImg.style.backgroundImage = "url('../img/3-tais-toi.jpg')";
-    movieDescription.textContent = "One prisoner tells to another that he hid a very large amount of money before the arrest. Together they manage to escape, but this is where their adventures are just beginning."
-  } else if (event.target == items[3]) {
-    posterImg.style.backgroundImage = "url('../img/4-stealmillion.jpg')";
-    movieDescription.textContent = "A woman who must steal a statue from a Paris museum to help conceal her father's art forgeries is assisted by a man."
-  } else if (event.target == items[4]) {
-    posterImg.style.backgroundImage = "url('../img/5-Womaningold.jpg')";
-    movieDescription.textContent = "Maria Altmann, an octogenarian Jewish refugee, takes on the Austrian government to recover artwork she believes rightfully belongs to her family."
+  for (let i = 0; i <= 7; i++) {
+    if(event.target == items[i]){
+      let randomSource = "photo" + (i+1) + ".jpg";
+      console.log(randomSource);
+      posterImg.style.backgroundImage = `url('../img/${randomSource}')`;
+    }
+    
   }
+  // if (event.target == items[0]) {
+  //   posterImg.style.backgroundImage = "url('../img/1-Friends.jpg')";
+  // } else if (event.target == items[1]) {
+  //   posterImg.style.backgroundImage = "url('../img/2-brother.jpg')";
+  //   movieDescription.textContent = "In the deep south during the 1930s, three escaped convicts search for hidden treasure while a relentless lawman pursues them."
+  // } else if (event.target == items[2]) {
+  //   posterImg.style.backgroundImage = "url('../img/3-tais-toi.jpg')";
+  //   movieDescription.textContent = "One prisoner tells to another that he hid a very large amount of money before the arrest. Together they manage to escape, but this is where their adventures are just beginning."
+  // } else if (event.target == items[3]) {
+  //   posterImg.style.backgroundImage = "url('../img/4-stealmillion.jpg')";
+  //   movieDescription.textContent = "A woman who must steal a statue from a Paris museum to help conceal her father's art forgeries is assisted by a man."
+  // } else if (event.target == items[4]) {
+  //   posterImg.style.backgroundImage = "url('../img/5-Womaningold.jpg')";
+  //   movieDescription.textContent = "Maria Altmann, an octogenarian Jewish refugee, takes on the Austrian government to recover artwork she believes rightfully belongs to her family."
+  // }
+  
+
+
 }
 //hide the description box
 const descriptionBox = document.querySelector(".description-box");
@@ -36,9 +47,11 @@ descriptionBox.classList.add("hideDescription");
 const posterImg = document.querySelector(".poster");
 const movieDescription = document.querySelector("p");
 
+
 //Event Listeners
 items.forEach((item) => {
   item.addEventListener("mouseover", changeBackground);
   item.addEventListener("click", displayDescr);
   item.addEventListener("mouseout", backToDefault);
 });
+
