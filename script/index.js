30150133bbb7057f6751914638ddb67863d4bf03
Simona <see.moona@yahoo.com>
2020-04-30 13:26:23 +0200

    on click

--- script/index.js
+++ script/index.js
@@ -9,17 +9,24 @@ function changeBackground() {
 function backToDefault() {
   this.style.backgroundColor = "#ffffff";
   this.style.color = "#263f44";
+  
 }
 function displayDescr() {
     this.style.backgroundColor = "#015668";
     this.style.color = "#ffd369";
+    descriptionBox.classList.toggle("displayDescription");
+    
   }
+function changeClass(){
 
+}
 
 //hide the description box
 const descriptionBox = document.querySelector(".description-box")
 console.log(descriptionBox)
-descriptionBox.classList.add("hideDescription", "displayDescription");
+descriptionBox.classList.add("hideDescription");
+
+// "displayDescription"
 
 
 items.forEach((item) => {
